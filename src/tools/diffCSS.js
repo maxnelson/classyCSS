6aac8a459414e8cc97582cc6333f3caee59ac21b
maxnelson <maxnelson88@gmail.com>
2024-08-23 13:39:25 -0700

    added diff tool to identify coverage for use in projects

---
+++ src/tools/diffCSS.js
@@ -0,0 +1,25 @@
+import { readFileSync, writeFileSync } from "fs";
+
+export const diffCSS = () => {
+  // Read the contents of the two CSS files
+  const cssFile1 = readFileSync("dist/main-version1.css", "utf8");
+  const cssFile2 = readFileSync("dist/main.css", "utf8");
+  // Extract classnames using a regex
+  const extractClassnames = (cssContent) => {
+    const classnames = cssContent.match(/\.\w[\w-]*/g);
+    return new Set(classnames);
+  };
+  const classnamesFile1 = extractClassnames(cssFile1);
+  const classnamesFile2 = extractClassnames(cssFile2);
+  // Find the classnames in file1 that are not in file2
+  const uniqueClassnames = [...classnamesFile1].filter(
+    (cn) => !classnamesFile2.has(cn)
+  );
+  // Write the array of unique classnames to the JSON file
+  writeFileSync(
+    "dist/diffedCSS.json",
+    JSON.stringify(uniqueClassnames, null, 2),
+    "utf8"
+  );
+};
+diffCSS();
